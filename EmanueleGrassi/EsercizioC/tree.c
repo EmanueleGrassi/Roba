d97d7c27b67bc856fa78f2ba0fbd59b297f0ea0f
Emanuele Grassi <emanuele.grassi@live.com>
2014-01-30 17:28:26 +0100

    fix

--- EmanueleGrassi/EsercizioC/tree.c
+++ EmanueleGrassi/EsercizioC/tree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "tree.h"
 
 int cercaParola(tree* t, char* str)
@@ -8,12 +9,21 @@ int cercaParola(tree* t, char* str)
     else
     {
         if (t->info == str[0])
-            return (cercaParola(t->left, str+1) || cercaParola(t->right, str+1));
+            return checkParola(t->left, str+1) || checkParola(t->right, str+1);
+        else
+            return (cercaParola(t->left, str) || cercaParola(t->right, str));
+    }
+}
+
+int checkParola(tree* t, char* str)
+{
+    if (str[0] == '\0') return 1;
+    if (t == NULL) return 0;
+    else
+    {
+        if (t->info == str[0])
+            return checkParola(t->left, str+1) || checkParola(t->right, str+1);
         else
             return 0;
     }
 }
-
-
-
-
